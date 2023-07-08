ee3b87b812373b9459eeceff600b7c4bd7ac0169
Trent <2771466+tbillington@users.noreply.github.com>
2023-07-08 12:55:09 +1000

    remove debug code

--- GMTK Game Jam/Assets/Scripts/Character.cs
+++ GMTK Game Jam/Assets/Scripts/Character.cs
@@ -18,21 +18,6 @@ public class Character : MonoBehaviour {
   void Update() {
     playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-    Debug.Log(playerInput);
-
-    // if (Input.GetKey(KeyCode.W)) {
-    //   playerInput += Vector3.forward;
-    // }
-    // if (Input.GetKey(KeyCode.S)) {
-    //   playerInput += Vector3.back;
-    // }
-    // if (Input.GetKey(KeyCode.A)) {
-    //   playerInput += Vector3.left;
-    // }
-    // if (Input.GetKey(KeyCode.D)) {
-    //   playerInput += Vector3.right;
-    // }
-
     if (rb.velocity.magnitude > 1f) {
       visualBody.rotation = Quaternion.LookRotation(rb.velocity);
     }
